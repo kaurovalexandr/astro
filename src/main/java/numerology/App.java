d9f9e9451a39fc64c2b4910b9b5e12f4e4362c1a
“Kaurov <alexandr.kaurov@pashapay.az>
2022-06-29 07:51:09 +0300

    calculate assembly point

--- src/main/java/numerology/App.java
+++ src/main/java/numerology/App.java
@@ -10,8 +10,8 @@ import java.util.HashMap;
 public class App {
     @SneakyThrows
     public static void main(String[] args) {
-        String fullName = "Макаров Тимур Александрович";
-        String birthday = "17.05.2021";
+        String fullName = "Каурова настя Сергеевна";
+        String birthday = "03.04.1994";
         HashMap<String, Integer> dictionary = Reader.getDictionary();
         InfoAboutName infoAboutName = AstroСalculator.getMagicNames(fullName, dictionary);
         System.out.println("полное имя: " + fullName);
